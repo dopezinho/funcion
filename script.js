e86b7441f7aa3c4a613f25ee737fda50707f435e
dopezinho <opugnero@gmail.com>
2022-12-04 14:53:23 -0300

    return

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-function 
+function presentinho (bufunfa, quantoe) {
+    if (bufunfa < quantoe){
+        return 'num tateno bro :('
+    } else {
+        return 'precisa memo compra essa parada?'
+    }
+}
+
+presentinho(50, 90)
