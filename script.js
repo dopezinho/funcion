aa958d654ab7f20d3152828543df7888e4b808f0
dopezinho <opugnero@gmail.com>
2022-12-04 15:11:17 -0300

    jururu beba

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// FUNÇÕES
+
+
 function presentinho (bufunfa, quantoe) {
     if (bufunfa < quantoe){
         return 'num tateno bro :('
@@ -8,3 +11,23 @@ function presentinho (bufunfa, quantoe) {
 
 let vem = presentinho(10, 90)
 console.log(vem)
+
+// EVENTOS DOM
+
+function clicae() {
+    main.style.background = '#fff'
+    main.innerText = 'TROXA'
+    main.style.color = 'red'
+}
+
+function dendelish() {
+    main.style.background = 'pink'
+    main.style.color = 'red'
+    main.innerText = 'vem ni mim bb clica no meu corpinho'
+}
+
+function foradelish() {
+    main.style.background = 'black'
+    main.style.color = 'brown'
+    main.innerText = 'volta pra mim s2'
+}
